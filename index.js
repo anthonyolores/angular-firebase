3c44d7aa0c9cc2d492cdb1761d67c30f0cda004c
anthonyolores <tonyoolores@gmail.com>
2018-05-27 20:27:38 +1200

    ..

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(express.static(path.join(__dirname, '/dist')));
 
 
 //subscribe route
-app.post('/',(request, response) => {
-});
+// app.post('/',(request, response) => {
+// });
 
 const port = process.env.PORT || '8080';
 app.listen(port, () => console.log(`Server running on port ${port}`));
